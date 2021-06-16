75a63c2f067bd4ecdcfb00be16697553955dfacc
flyhighhigh <63686895+flyhighhigh@users.noreply.github.com>
2021-06-16 16:09:25 +0800

    修正中文字無法反轉問題

--- 21_reverse.c
+++ 21_reverse.c
@@ -5,10 +5,34 @@
 #include<ctype.h>
 #define maxbuf 3
 
+typedef unsigned char uchar;
+
+void swapchar(int fd,int a,int b){
+
+    uchar buf_a[maxbuf];
+    uchar buf_b[maxbuf];
+
+    //讀取左元素buf1
+    lseek(fd,a,SEEK_SET);
+    read(fd,buf_a,1);
+        
+
+    //讀取右元素buf2
+    lseek(fd,b,SEEK_SET);
+    read(fd,buf_b,1);
+
+    //右元素貼到左元素
+    lseek(fd,a,SEEK_SET);
+    write(fd,buf_b,1);
+
+    //左元素放到右元素
+    lseek(fd,b,SEEK_SET);
+    write(fd,buf_a,1);
+}
+
 int main(int argc,char** argv){
 
-    char buf1[maxbuf];
-    char buf2[maxbuf];
+    uchar buf[maxbuf];
     int fd;
 
     if(argc!=2){
@@ -24,26 +48,19 @@ int main(int argc,char** argv){
     }
     int idx=0;
     int len=lseek(fd,0,SEEK_END);
-    printf("len=%d\n",len);
+    
+    for(int i=0;i<len;i++){
+        lseek(fd,i,SEEK_SET);
+        read(fd,buf,1);
+        if(buf[0]>=224){
+            swapchar(fd,i,i+2);
+            i+=2;
+        }
+    }
+    
     while(idx<len/2){//須注意read會自動將游標往前
 
-        //讀取左元素buf1
-        lseek(fd,idx,SEEK_SET);
-        read(fd,buf1,1);
-        
-
-        //讀取右元素buf2
-        lseek(fd,-1*idx-1,SEEK_END);
-        read(fd,buf2,1);
-
-        //右元素貼到左元素
-        lseek(fd,idx,SEEK_SET);
-        write(fd,buf2,1);
-
-        //左元素放到右元素
-        lseek(fd,-1*idx-1,SEEK_END);
-        write(fd,buf1,1);
-
+        swapchar(fd,idx,len-idx-1);
         idx++;
     }
     close(fd);
